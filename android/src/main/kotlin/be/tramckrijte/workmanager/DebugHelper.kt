9ad87262f0c39dfd79aa9cfd38b859a70549f7d9
cahyowhy <cahyowhy01@gmail.com>
2021-07-05 10:41:44 +0700

    change debug message

--- android/src/main/kotlin/be/tramckrijte/workmanager/DebugHelper.kt
+++ android/src/main/kotlin/be/tramckrijte/workmanager/DebugHelper.kt
@@ -13,10 +13,10 @@ import java.util.concurrent.TimeUnit.MILLISECONDS
 
 object ThumbnailGenerator {
     fun mapResultToEmoji(result: ListenableWorker.Result): String =
-        when (result) {
-            is ListenableWorker.Result.Success -> "\uD83C\uDF89"
-            else -> "\uD83D\uDD25"
-        }
+            when (result) {
+                is ListenableWorker.Result.Success -> "\uD83C\uDF89"
+                else -> "\uD83D\uDD25"
+            }
 
     val workEmoji get() = listOf("\uD83D\uDC77\u200D♀️", "\uD83D\uDC77\u200D♂️").random()
 }
@@ -25,57 +25,58 @@ object DebugHelper {
     private const val debugChannelId = "WorkmanagerDebugChannelId"
     private const val debugChannelName = "A helper channel to debug your background tasks."
     private val debugDateFormatter =
-        DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM)
+            DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM)
 
     private val currentTime get() = debugDateFormatter.format(Date())
 
     private fun mapMillisToSeconds(milliseconds: Long) =
-        "${MILLISECONDS.toSeconds(milliseconds)} seconds."
+            "${MILLISECONDS.toSeconds(milliseconds)} seconds."
 
     fun postTaskCompleteNotification(
-        ctx: Context,
-        threadIdentifier: Int,
-        dartTask: String,
-        payload: String? = null,
-        fetchDuration: Long,
-        result: ListenableWorker.Result
+            ctx: Context,
+            threadIdentifier: Int,
+            dartTask: String,
+            payload: String? = null,
+            fetchDuration: Long,
+            result: ListenableWorker.Result
     ) {
         postNotification(
-            ctx,
-            threadIdentifier,
-            "${ThumbnailGenerator.workEmoji} $currentTime",
-            """
-                    • Result: ${ThumbnailGenerator.mapResultToEmoji(result)} ${result.javaClass.simpleName}
-                    • dartTask: $dartTask
-                    • inputData: ${payload ?: "not found"}
-                    • Elapsed time: ${mapMillisToSeconds(fetchDuration)}
-            """.trimIndent()
+                ctx,
+                threadIdentifier,
+                "${ThumbnailGenerator.workEmoji} $currentTime",
+                /*"""
+                        • Result: ${ThumbnailGenerator.mapResultToEmoji(result)} ${result.javaClass.simpleName}
+                        • dartTask: $dartTask
+                        • inputData: ${payload ?: "not found"}
+                        • Elapsed time: ${mapMillisToSeconds(fetchDuration)}
+                """.trimIndent()*/
+                "Run $dartTask"
         )
     }
 
     fun postTaskStarting(
-        ctx: Context,
-        threadIdentifier: Int,
-        dartTask: String,
-        payload: String? = null,
-        callbackHandle: Long,
-        callbackInfo: FlutterCallbackInformation?,
-        dartBundlePath: String?
+            ctx: Context,
+            threadIdentifier: Int,
+            dartTask: String,
+            payload: String? = null,
+            callbackHandle: Long,
+            callbackInfo: FlutterCallbackInformation?,
+            dartBundlePath: String?
     ) {
         postNotification(
-            ctx,
-            threadIdentifier,
-            "${ThumbnailGenerator.workEmoji} $currentTime",
-            """
-                • dartTask: $dartTask
-                • inputData: ${payload ?: "not found"}
-                • callbackHandle: $callbackHandle 
-                • callBackName: ${callbackInfo?.callbackName ?: "not found"}
-                • callbackClassName: ${callbackInfo?.callbackClassName ?: "not found"}
-                • callbackLibraryPath: ${callbackInfo?.callbackLibraryPath ?: "not found"}
-                • dartBundlePath: $dartBundlePath"
-            """.trimIndent()
-
+                ctx,
+                threadIdentifier,
+                "${ThumbnailGenerator.workEmoji} $currentTime",
+                /*"""
+                    • dartTask: $dartTask
+                    • inputData: ${payload ?: "not found"}
+                    • callbackHandle: $callbackHandle
+                    • callBackName: ${callbackInfo?.callbackName ?: "not found"}
+                    • callbackClassName: ${callbackInfo?.callbackClassName ?: "not found"}
+                    • callbackLibraryPath: ${callbackInfo?.callbackLibraryPath ?: "not found"}
+                    • dartBundlePath: $dartBundlePath"
+                """.trimIndent()*/
+                "Run $dartTask"
         )
     }
 
@@ -84,16 +85,16 @@ object DebugHelper {
             createNotificationChannel()
 
             notify(
-                messageId,
-                NotificationCompat.Builder(ctx, debugChannelId)
-                    .setContentTitle(title)
-                    .setContentText(contentText)
-                    .setStyle(
-                        NotificationCompat.BigTextStyle()
-                            .bigText(contentText)
-                    )
-                    .setSmallIcon(android.R.drawable.stat_notify_sync)
-                    .build()
+                    messageId,
+                    NotificationCompat.Builder(ctx, debugChannelId)
+                            .setContentTitle(title)
+                            .setContentText(contentText)
+                            .setStyle(
+                                    NotificationCompat.BigTextStyle()
+                                            .bigText(contentText)
+                            )
+                            .setSmallIcon(android.R.drawable.stat_notify_sync)
+                            .build()
             )
         }
     }
@@ -101,11 +102,11 @@ object DebugHelper {
     private fun NotificationManager.createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             createNotificationChannel(
-                NotificationChannel(
-                    debugChannelId,
-                    debugChannelName,
-                    NotificationManager.IMPORTANCE_DEFAULT
-                )
+                    NotificationChannel(
+                            debugChannelId,
+                            debugChannelName,
+                            NotificationManager.IMPORTANCE_DEFAULT
+                    )
             )
         }
     }
